a0a702cd5d09214d544f784b62be9abecd39f295
serrt <1163816051@qq.com>
2018-12-09 17:39:32 +0800

    face key

--- config/face.php
+++ config/face.php
@@ -2,11 +2,11 @@
 
 return [
     'plus' => [
-        'api_key' => 'ForCx47MxWWZcs3DSwhn0RNsyQe4b2oh',
-        'api_secret' => 'L_f3AJ8vZGq1dh986ouV-wnCzEIJUPPJ'
+        'api_key' => '',
+        'api_secret' => ''
     ],
     'id' => [
-        'api_key' => 'VV2Oz6ybpACX5ewT-HrMp8gCFUk2ozFB',
-        'api_secret' => 'qAQbWa3iZYj1C0v-tQuygZPg9PzWQ8Wo'
+        'api_key' => '',
+        'api_secret' => ''
     ]
 ];
